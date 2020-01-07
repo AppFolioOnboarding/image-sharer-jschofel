b2c6b1ab24fb819a97b8adfb9290eaf8af5b367a
Jacob Schofel <jacob.schofel@appfolio.com>
2020-01-07 14:24:56 -0800

    JS - add image form + show

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,19 @@
+class ImagesController < ApplicationController
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  def new; end
+
+  def create
+    @image = Image.new(image_params)
+    @image.save
+    redirect_to @image
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:url)
+  end
+end
---
+++ test/controllers/image_form_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ImageFormControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
