2cf1e98504228fb2b2d87b92ee5449dd8bb8dbec
Jacob Schofel <jacob.schofel@appfolio.com>
2020-01-07 17:35:59 -0800

    JS - removed some unused routes + added NOT NULL to Images

--- db/migrate/20200107190051_create_images.rb
+++ db/migrate/20200107190051_create_images.rb
@@ -1,8 +1,7 @@
 class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
-      t.string :url
-
+      t.string :url, null: false
       t.timestamps
     end
   end
