c508595e4a6fdcdf53d2a9be9895df2136c89b5e
Jacob Schofel <jacob.schofel@appfolio.com>
2020-01-08 12:12:11 -0800

    JS - fixed issues with removing fixtures

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -1,16 +1,13 @@
 require 'test_helper'
 
 class ImagesControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @image = Image.new
-  end
-
   test 'should get new' do
     get new_image_url
     assert_response :success
   end
 
   test 'should create image' do
+    Image.create(url: 'https://')
     assert_difference('Image.count') do
       post images_url, params: { image: { url: 'https://www.website.com' } }
     end
@@ -19,6 +16,7 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should show image' do
+    Image.create(url: 'https://')
     get image_url(Image.last)
     assert_response :success
   end
