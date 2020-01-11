9bbadb88661656134364df3db5ec6aeb51269308
Jacob Schofel <jacob.schofel@appfolio.com>
2020-01-10 17:34:03 -0800

    JS - updated controller test for tags

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -12,12 +12,19 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
 
   test 'should be descending order html' do
     img_num = 10
-    img_num.times { |n| Image.create(url: "https://www.images.com/#{n}") }
+    img_num.times do |n|
+      Image.create(url: "https://www.images.com/#{n}", tag_list: "Outdoors, Tag#{n}")
+    end
     get root_url
-    assert_select 'img', count: 10
-    assert_select 'img' do |elements|
+    assert_select 'div', attributes: { class: 'image-tag-wrapper' }, count: 10
+    assert_select 'div', attributes: { class: 'image-tag-wrapper' } do |elements|
       elements.each do |element|
-        assert_equal "https://www.images.com/#{img_num - 1}", element.attribute('src').value
+        assert_select element, 'img', count: 1 do |img_html|
+          assert_equal "https://www.images.com/#{img_num - 1}", img_html.attribute('src').value
+        end
+        assert_select element, 'p', count: 1 do |p_html|
+          assert_equal "Outdoors, Tag#{img_num - 1}", p_html.text
+        end
         img_num -= 1
       end
     end
